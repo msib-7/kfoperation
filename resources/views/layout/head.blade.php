800ac49a888e7674ad6b905d0b414a9204a20344
Ferdy Rahmat <ferdyyrahmat@gmail.com>
2024-10-29 17:51:36 +0700

    Create head.blade.php

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- title and description -->
+<title>Home | Homepage KalbeFarma</title>
+<!-- meta data -->
+<link rel="shortcut icon" href="{{ asset('assets/img/logo_only.png') }}" type="image/x-icon">
+
+<!-- css -->
+<link rel="stylesheet" href="{{ asset('assets/css/output.css') }}">
+@vite(['resources/css/app.css','resources/js/app.js'])
+
+<!-- js -->
+<script defer src="{{ asset('assets/js/index.js') }}"></script>
+<script defer src="{{asset('assets/js/observer.js')}}"></script>
