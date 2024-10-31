5b04b8be47eee6836d845e0cdde5a551f4d7421f
Ferdy Rahmat <ferdyyrahmat@gmail.com>
2024-11-01 00:55:45 +0700

    Update page-qs.blade.php

--- resources/views/cms/page/page-qs.blade.php
+++ resources/views/cms/page/page-qs.blade.php
@@ -109,12 +109,11 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
+                <h5 class="modal-title">Caption</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <span id="text-caption"></span>
-            </div>
-            <div class="modal-footer">
             </div>
         </div>
     </div>
